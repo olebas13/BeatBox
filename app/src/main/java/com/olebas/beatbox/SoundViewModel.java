3a4bb87e020a3573f597ac5a20ade35789c251e8
olebas13 <o.nevoyt13@gmail.com>
2018-10-24 18:10:57 +0300

    listing 20.25

---
+++ app/src/main/java/com/olebas/beatbox/SoundViewModel.java
@@ -0,0 +1,23 @@
+package com.olebas.beatbox;
+
+public class SoundViewModel {
+
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    public String getTitle() {
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+    }
+}
