430fc64b1595bd82b4116ec36300281bf50da4ec
olebas13 <o.nevoyt13@gmail.com>
2018-10-25 16:56:35 +0300

    listing 21.9

---
+++ app/src/test/java/com/olebas/beatbox/SoundViewModelTest.java
@@ -0,0 +1,21 @@
+package com.olebas.beatbox;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+public class SoundViewModelTest {
+
+    private BeatBox mBeatBox;
+    private Sound mSound;
+    private SoundViewModel mSubject;
+
+    @Before
+    public void setUp() throws Exception {
+        mBeatBox = mock(BeatBox.class);
+        mSound = new Sound("assetPath");
+        mSubject = new SoundViewModel(mBeatBox);
+        mSubject.setSound(mSound);
+    }
+}
